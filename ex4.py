07302d3a71703bd8daa9380343ea2fbb49b45009
imad <147118339+eddineimad@users.noreply.github.com>
2023-10-19 03:36:51 +0200

    Add files via upload

---
+++ ex4.py
@@ -0,0 +1,10 @@
+for i in range( 0,3) :  
+    mot=input("Type the password")
+    if mot="DEV@2023" :
+        print("Bonjour")
+if mot!="DEV@2023" :
+    j=input("Type name of ur favorite movie")
+    if j="" :
+        print("Matrix")
+    else :
+        print ("false")
